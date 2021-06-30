e75e9ef26c95664d7250efb298087e4f7fbcaa65
wmartzh <wmartzh@gmail.com>
2021-06-29 23:02:01 -0600

    [Fix] Refactor Register page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
-import { Form, Card, Button, Columns, Block, Notification, Hero } from 'react-bulma-components';
+import {
+  VStack,
+  HStack,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  InputGroup,
+  Input,
+  InputRightElement,
+  Box,
+  Text,
+  Container,
+  Button,
+  Icon,
+  Alert,
+  AlertIcon,
+  AlertDescription,
+} from '@chakra-ui/react';
 import { fetchRegister } from '../api/authService';
 const Register = () => {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [showPass, setShowPass] = useState(false);
 
   const [error, setError] = useState('');
   const handleRegister = async (e) => {
@@ -42,86 +60,81 @@ const Register = () => {
   };
   return (
     <>
-      <Hero>
-        <Hero.Body size="fullheight">
-          <Columns centered={true}>
-            <Columns.Column size="one-quarter">
-              <Card>
-                <Card.Content>
-                  <form>
-                    <h2 className="title is-4 has-text-centered">Register</h2>
-                    <Block>
-                      {error && (
-                        <Notification color="danger">
-                          {error} <Button remove />
-                        </Notification>
-                      )}
-                    </Block>
-                    <Form.Field>
-                      <Form.Label>Username</Form.Label>
-                      <Form.Control>
-                        <Form.Input
-                          color={username && username !== '' ? 'success' : 'danger'}
-                          type="text"
-                          value={username}
-                          onChange={(e) => setUsername(e.target.value)}
-                        ></Form.Input>
-                      </Form.Control>
-                    </Form.Field>
-                    <Form.Field>
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control>
-                        <Form.Input
-                          color={email && email !== '' ? 'success' : 'danger'}
-                          type="text"
-                          value={email}
-                          onChange={(e) => setEmail(e.target.value)}
-                        ></Form.Input>
-                      </Form.Control>
-                    </Form.Field>
-                    <Form.Field>
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control>
-                        <Form.Input
-                          color={password && password !== '' ? 'success' : 'danger'}
-                          type="password"
-                          value={password}
-                          onChange={(e) => setPassword(e.target.value)}
-                        ></Form.Input>
-                      </Form.Control>
-                    </Form.Field>
-                    <Form.Field>
-                      <Form.Label>Confirm Password</Form.Label>
-                      <Form.Control>
-                        <Form.Input
-                          color={
-                            passwordConfirm &&
-                            passwordConfirm !== '' &&
-                            password === passwordConfirm
-                              ? 'success'
-                              : 'danger'
-                          }
-                          type="password"
-                          value={passwordConfirm}
-                          onChange={(e) => setPasswordConfirm(e.target.value)}
-                        ></Form.Input>
-                      </Form.Control>
-                      {password !== passwordConfirm && (
-                        <Form.Help color="danger">Password doesn't match</Form.Help>
-                      )}
-                    </Form.Field>
-                    <Button.Group align="right">
-                      <Button color="success" onClick={handleRegister}>
-                        Register
-                      </Button>
-                    </Button.Group>
-                  </form>
-                </Card.Content>
-              </Card>
-            </Columns.Column>
-          </Columns>
-        </Hero.Body>
-      </Hero>
+      <Container>
+        <Box boxShadow="xs" p="10%" marginTop="25%" textAlign="center">
+          <Text fontSize="xl"> Register </Text>
+          <form>
+            <VStack>
+              {error && (
+                <Alert status="error">
+                  <AlertIcon />
+                  <AlertDescription>{error}</AlertDescription>
+                </Alert>
+              )}
+              <FormControl isInvalid={!username || username === ''}>
+                <FormLabel htmlFor="username">username</FormLabel>
+                <Input
+                  id="username"
+                  placeholder="username"
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+                <FormErrorMessage>Please type a username</FormErrorMessage>
+              </FormControl>
+              <FormControl isInvalid={!email || email === ''}>
+                <FormLabel htmlFor="email">Email</FormLabel>
+                <Input id="email" placeholder="email" onChange={(e) => setEmail(e.target.value)} />
+                <FormErrorMessage>Please type a email</FormErrorMessage>
+              </FormControl>
+
+              <FormControl isInvalid={!password && password === ''}>
+                <FormLabel htmlFor="password">Password</FormLabel>
+                <InputGroup>
+                  <Input
+                    id="password"
+                    placeholder="password"
+                    type={showPass ? 'text' : 'password'}
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={() => setShowPass(!showPass)}>
+                      {showPass ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormErrorMessage>Please type a password</FormErrorMessage>
+              </FormControl>
+              <FormControl
+                isInvalid={
+                  !passwordConfirm || passwordConfirm === '' || password !== passwordConfirm
+                }
+              >
+                <FormLabel htmlFor="passwordConfirm">Confirm password</FormLabel>
+                <InputGroup>
+                  <Input
+                    id="passwordConfirm"
+                    placeholder="passwordConfirm"
+                    type={showPass ? 'text' : 'passwordConfirm'}
+                    onChange={(e) => setPasswordConfirm(e.target.value)}
+                  />
+                  <InputRightElement width="4.5rem">
+                    <Button h="1.75rem" size="sm" onClick={() => setShowPass(!showPass)}>
+                      {showPass ? 'Hide' : 'Show'}
+                    </Button>
+                  </InputRightElement>
+                </InputGroup>
+                <FormErrorMessage>
+                  {password !== passwordConfirm
+                    ? 'Password does not match'
+                    : 'Please Confirm password'}
+                </FormErrorMessage>
+              </FormControl>
+              <Button onClick={handleRegister} colorSchema="teal" size="md">
+                Register
+              </Button>
+            </VStack>
+          </form>
+        </Box>
+      </Container>
     </>
   );
 };
